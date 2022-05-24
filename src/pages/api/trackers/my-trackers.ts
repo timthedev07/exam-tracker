6f76ba6e4c2260f811500bca2a38ddb9608dfbda
Tim <65774333+timthedev07@users.noreply.github.com>
2022-05-24 12:04:30 +0000

    complete handler

--- src/pages/api/trackers/my-trackers.ts
+++ src/pages/api/trackers/my-trackers.ts
@@ -2,11 +2,17 @@ import { NextApiHandler } from "next";
 import { getSession } from "next-auth/react";
 import { withAuth } from "../../../lib/middleware/authGuard";
 import { withMethodGuard } from "../../../lib/middleware/methodGuard";
+import { getUserTrackers } from "../../../mongodb/functions/getUserTrackers";
 
 const handler: NextApiHandler = async (req, res) => {
   const session = (await getSession({ req }))!;
 
   const userId = session.userId;
+
+  const items = await getUserTrackers(userId);
+
+  res.send(items);
+  res.end();
 };
 
 export default withMethodGuard(withAuth(handler), "GET");
